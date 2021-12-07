d77516b5438c616f3f0fbe91e58b4d0cbb37c215
타래 <bae94157617@gmail.com>
2021-12-08 00:43:56 +0900

    12_08-12:43

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,11 @@
-import styled, { keyframes } from "styled-components";
-
-const Wrapper = styled.div`
-  width: 100vw;
-  height: 100vh;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  background-color: #262626;
-`;
-
-const TurnnigBox = keyframes`
-  from{
-    transform: rotate(0deg);
-  }
-  to{
-    transform: rotate(360deg);
-  }
-`;
-
-const Box = styled.div`
-  background-color: skyblue;
-  width: 200px;
-  height: 100px;
-  border-radius: 20px;
-  border: 5px solid white;
-  animation: ${TurnnigBox} 2s linear infinite;
-`;
+import Circle from "./Circle";
 
 function App() {
   return (
-    <Wrapper>
-      <Box />
-    </Wrapper>
+    <div>
+      <Circle bgColor="skyblue" />
+      <Circle bgColor="#262626" />
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,10 @@
-import { ThemeProvider } from "styled-components";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-const darkTheme = {
-  textColor: "whiteSmoke",
-  backgroundColor: "#262626",
-};
-
-const lightTheme = {
-  textColor: "#262626",
-  backgroundColor: "whiteSmoke",
-};
-
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
