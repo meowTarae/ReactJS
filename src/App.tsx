0f96c82f562a739100e5c68ecd53868e28c6a2e3
타래 <bae94157617@gmail.com>
2021-12-08 23:24:20 +0900

    Theory for TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
+import styled from "styled-components";
 
 function App() {
+  const Container = styled.div`
+    background-color: ${(p) => p.theme.bgColor};
+    span {
+      color: ${(p) => p.theme.textColor};
+    }
+  `;
+  // const guelZa = styled.h2`
+  //   color: ${(p) => p.theme.textColor};
+  // `;
+
   return (
-    <div>
-      <Circle bgColor="#F58025" borderColor="white" />
-      <Circle bgColor="skyblue" />
-    </div>
+    <Container>
+      <span>방가방가</span>
+    </Container>
   );
 }
 
